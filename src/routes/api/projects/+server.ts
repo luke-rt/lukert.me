b2e816951a2dbfaf4786a0afa1be1184bcf7178f
luke-rt <luke.rtong@gmail.com>
2023-08-20 23:35:59 -0400

    added new project + deps

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -14,8 +14,14 @@ export async function GET() {
 	projects.push({
 		title: "Asciiframe",
 		img: "asciiframe.png",
-		desc: "A command-line tool that can convert any video (or image) into a stream of ascii characters. Written with Rust and OpenCV.",
+		desc: "A command-line tool that can convert any video (or image) into a stream of ascii characters that can be played in the terminal. Written with Rust and Open Computer Vision.",
 		src_url: "https://github.com/luke-rt/asciiframe",
+	});
+	projects.push({
+		title: "Guess the Number OS (Kernel)",
+		img: "guess-the-number.png",
+		desc: "A bare metal, 32-bit kernel + custom bootloader written in C and Intel Assembly. All it does is play guess the number.",
+		src_url: "https://github.com/luke-rt/guess-the-number",
 	});
 
 	return json({
